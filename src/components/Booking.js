dd5e36fa16b54b71464b66aee851adad207c6c90
Nischal1111 <neupanenischal2@gmail.com>
2023-05-17 12:13:52 +0545

    Updated

---
+++ src/components/Booking.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../Css/Booking.css"
+
+const Booking = () => {
+  return (
+    <div>
+      Booking
+    </div>
+  )
+}
+
+export default Booking
--- src/pages/TrekDetail.js
+++ src/pages/TrekDetail.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React,{useRef,useState} from 'react'
 import "../Css/TrekDetail.css"
 
 import avatar from "../assets/avatar.jpg"
 
 import TrekData from '../Data/Trekdata'
+import Booking from '../components/Booking'
 
 import {useParams,Link} from "react-router-dom"
 
@@ -11,14 +12,34 @@ import {useParams,Link} from "react-router-dom"
 
 const TrekDetail = () => {
     const {id} = useParams();
+
+
+    const RevMsgref=useRef("");
+
+
+    const [tourRate, setTourRate]=useState(null)
+
+
     console.log(id)
+    
+    
+    const submithandler=(e)=>{
+        e.preventDefault();
+        const revText=RevMsgref.current.value;
+        alert(`${revText},${tourRate}`)
+    }
+
+
     const trek = TrekData.find((item)=>{
         return item.id===parseInt(id)
     })
+
+
     console.log(trek)
     const format = {day:'numeric',month:"long",year:"numeric"}
     return (<>
         <section>
+            <main className='left--div'>
             <div className="detail">
                 <div className='detail--img--div'>
                     <img src={trek.imgSrc} className='detail--img'/>
@@ -37,10 +58,10 @@ const TrekDetail = () => {
                             <i className="fa-solid fa-location-dot" style={{marginRight:"10px"}}></i>{trek.location}
                         </span>
                         <div className='detail--extra'>
-                            <span className='price'>
+                            <span className='price' style={{fontSize:"18px"}}>
                                 <i className="fa-solid fa-dollar-sign " style={{marginRight:"10px"}}></i>{trek.price} / per person
                             </span>
-                            <span style={{marginLeft:"70px",fontWeight:"200",}}>
+                            <span style={{marginLeft:"40px",fontWeight:"200",}}>
                                 <i className="fa-solid fa-user-group" style={{marginRight:"10px"}}></i>6-8 people per group
                             </span>
                         </div>
@@ -48,29 +69,28 @@ const TrekDetail = () => {
                         <p>{trek.desc}</p>
                     </div>
             </div>
-            </div>
             <div className='review'>
                 <h4>Customer Reviews (5 Reviews)</h4>
-                <form>
+                <form onSubmit={submithandler}>
                     <div className='rev--div'>
-                        <span>
+                        <span onClick={()=>setTourRate(1)}>
                             1 <i class="fa-solid fa-star"></i>
-                        </span>
-                        <span>
+                        </span >
+                        <span onClick={()=>setTourRate(2)}>
                             2 <i class="fa-solid fa-star"></i>
                         </span>
-                        <span>
+                        <span onClick={()=>setTourRate(3)}>
                             3 <i class="fa-solid fa-star"></i>
                         </span>
-                        <span>
+                        <span onClick={()=>setTourRate(4)}>
                             4 <i class="fa-solid fa-star"></i>
                         </span>
-                        <span>
-                            5 <i class="fa-solid fa-star-half-alt"></i>
+                        <span onClick={()=>setTourRate(5)}>
+                            5 <i class="fa-solid fa-star"></i>
                         </span>
                     </div>
                     <div className="review--input">
-                        <input type='text' placeholder='share your thoughts'/>
+                        <input type='text' placeholder='Share your thoughts' ref={RevMsgref} required/>
                         <button className="rev--btn" type='submit'>Submit</button>
                     </div>
                 </form>
@@ -79,18 +99,24 @@ const TrekDetail = () => {
                         <img src={avatar}/>
                         <div style={{width:"100%"}}>
                             <div style={{display:"flex",alignItems:"center",justifyContent:"space-between"}}>
-                                <div>
+                                <div className='rev--name'>
                                     <h3 style={{fontWeight:"200",fontSize:"22px",color:" #314f73"}}>Nischal Dai</h3>
-                                    <p>{new Date("05-15-2023").toLocaleDateString("en-US", format)}</p>
+                                    <p style={{fontSize:"14px",color:"#0c386e"}}>{new Date("05-15-2023").toLocaleDateString("en-US", format)}</p>
                                 </div>
                                 <span style={{display:"flex",alignItems:"center"}}>
                                     5 <i class="fa-solid fa-star"></i>
                                 </span>
                             </div>
+                            <h5>Was a thrilling adventure !!!</h5>
                         </div>
                     </div>
                 </div>
             </div>
+            </div>
+            </main>
+            <main className='right-div'>
+                
+            </main>
         </section>
     </>)
 }
