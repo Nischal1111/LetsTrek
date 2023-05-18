beecf8ce36dc7746ccf4b5899ea1f10b11f13fc2
Nischal1111 <neupanenischal2@gmail.com>
2023-05-18 21:09:16 +0545

    Updated

--- src/Data/Homedata.js
+++ src/Data/Homedata.js
@@ -2,7 +2,7 @@
 const Homedata=[
     {
         title:'On top of the world',
-        image:'https://images.pexels.com/photos/3419791/pexels-photo-3419791.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+        image:'https://images.pexels.com/photos/15497103/pexels-photo-15497103.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
         id:0,
         text:"Don't confuse hike with destination",
         url:'./trekkings',
@@ -12,7 +12,7 @@ const Homedata=[
     },
     {
         title:'Lets Trek',
-        image:'https://images.unsplash.com/photo-1490791539531-102a1e0beb7b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+        image:'https://images.pexels.com/photos/2930934/pexels-photo-2930934.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
         id:1,
         text:'Join us for thrilling treks',
         url:'./trekkings',
@@ -23,7 +23,7 @@ const Homedata=[
     {
         title:'We dream of Travel',
         id:2,
-        image:'https://images.unsplash.com/photo-1533005346585-b131e79eef3a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80',
+        image:'https://images.unsplash.com/photo-1506967534058-2dc0162a83d6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1770&q=80',
         text:'Vanity dies on Nature trails',
         url:'./trekkings',
         btnText:'Lets Go',
@@ -33,7 +33,7 @@ const Homedata=[
     {
         title:'Adventure Awaits',
         id:3,
-        image:'https://images.unsplash.com/photo-1670046452418-8ef8eb0877fc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+        image:'https://images.pexels.com/photos/1529881/pexels-photo-1529881.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
         text:"What are you waiting for ?",
         url:'./trekkings',
         btnText:'Travel with us',
--- src/components/Booking.js
+++ src/components/Booking.js
@@ -5,7 +5,16 @@ const Booking = ({trek}) => {
     console.log(trek)
 return (
     <div className='booking'>
-        <div className="booking--top" style={{display:"flex", alignItems:"center", justifyContent:"space-between"}}>Booking</div>
+        <div className="booking--top" style={{display:"flex", alignItems:"center", justifyContent:"space-between"}}>
+            <span style={{fontSize:"22px",color:"#314f73"}}><h3 style={{fontFamily:"geneva",color:"#5889c5",fontSize:"28px"}}>{trek.price}</h3>/ per person</span>
+            <span className='rate'>
+                <i className="fa-solid fa-star"></i>
+                <i className="fa-solid fa-star"></i>
+                <i className="fa-solid fa-star"></i>
+                <i className="fa-solid fa-star"></i>
+                <span><i className="fa-solid fa-star-half-alt"></i>4.6 (13)</span>
+            </span> 
+        </div>
     </div>
 )
 }
