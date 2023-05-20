3d2e6b00df8184d585c0b8d6be6dbef29e06cc35
Nischal1111 <neupanenischal2@gmail.com>
2023-05-21 04:01:05 +0545

    Updated

--- src/components/Booking.js
+++ src/components/Booking.js
@@ -11,7 +11,7 @@ const Booking = ({trek}) => {
 
 return (
     <div className='booking'>
-        <div className="booking--top" style={{display:"flex", alignItems:"center", justifyContent:"space-between"}}>
+        <div className="booking--top" style={{display:"flex", alignItems:"center", justifyContent:"space-between",marginTop:".7rem"}}>
             <h3 style={{fontFamily:"geneva",color:"aliceblue",fontSize:"24px"}}>{trek.price}</h3>
             <span className='rate'>
                 <span><i className="fa-solid fa-star"></i>4.6 (13)</span>
@@ -20,19 +20,19 @@ return (
         <div className='booking-form'>
             <h4>Register for Trek</h4>
             <form className='form--form'>
-                <label>Full Name*</label><br/>
+                <label>Full Name</label><br/>
                 <input required type='text' id='fullname' placeholder='Full Name' onChange={handleChange} autoComplete='off'/><br/>
-                <label>E-mail*</label><br/>
+                <label>E-mail</label><br/>
                 <input required type='email' id='email' placeholder='Email' onChange={handleChange}autoComplete='off'/><br/>
-                <label>Phone*</label><br/>
+                <label>Phone</label><br/>
                 <input required type='text' id='phone' placeholder='Phone' onChange={handleChange} autoComplete='off'/><br/>
                 <div className='form--date' style={{marginTop:".7rem"}}><br/>
                     <div style={{display:"flex",flexDirection:"column"}}>
-                    <label>Travel Date*</label><br/>
+                    <label>Travel Date</label><br/>
                     <input required placeholder='' type='date' id='book' onChange={handleChange} autoComplete='off'/><br/>
                     </div>
                     <div style={{display:"flex",flexDirection:"column",marginLeft:"4rem"}}>
-                    <label>People*</label><br/>
+                    <label>People</label><br/>
                     <input required placeholder='' type='number' id='group' onChange={handleChange} autoComplete='off' /><br/><br/>
                     </div>
                 </div>  
