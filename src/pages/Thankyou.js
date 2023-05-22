06453c938a290938297c995072f9e3ed17a82c55
Nischal1111 <neupanenischal2@gmail.com>
2023-05-23 00:32:40 +0545

    Updated

--- src/pages/Thankyou.js
+++ src/pages/Thankyou.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import {Link} from "react-router-dom"
+import "../Css/thank.css"
+import {AiOutlineCheckCircle} from "react-icons/ai"
 
 const Thankyou = () => {
-  return (
-    <div>
-      Thank you
+return (
+    <div className='thank'>
+        <span><AiOutlineCheckCircle className="icon"/></span>
+        <h1>Thank you</h1>
+        <p className='p1'>Your Trek has been booked.</p>
+        <p className='p2'>We will reach you shortly.</p>
+        <button>
+            <Link to="/" style={{textDecoration:"none",color:"white"}}>Back to Home</Link>
+        </button>
     </div>
-  )
+)
 }
 
 export default Thankyou
